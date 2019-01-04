1ecc349309349d39832fbd560e04dfef32b3fdcf
shawngu86 <x.gu@hotmail.co.uk>
2019-01-04 16:32:23 +0800

    Update xml_parser.py

--- xml_parser.py
+++ xml_parser.py
@@ -4,8 +4,8 @@ import re, os,datetime
 import graphviz as gv
 from numpy import nan
 
-path=r'C:\Users\e650188\Desktop\automation_files\xml - Copy'
-
+path=r'C:\Users\e650188\Desktop\automation_files\xml_part_c'
+mart_name='Part_C'
 ################ Prepare xml ################
 
 for r,d,files in os.walk(path):
@@ -24,7 +24,7 @@ for r,d,files in os.walk(path):
                     textobj.write(line)
 textobj.close()
 
-################## Parse xml #################
+print('Parse xml')################## Parse xml #################
 def iter_docs(author,element='Table'):
     author_attr = author.attrib
     for doc in author.iter(element):
@@ -57,7 +57,7 @@ df.level=df.level.map('{:.0f}'.format)  # int format
 
 df.loc[df['level']=='nan','level'] = df[df['level']=='nan']['R_num']  # replace nan with R_num to distinct feeds
 
-################## GRAPH #################
+print('GRAPH')################## GRAPH #################
 os.environ["PATH"] += os.pathsep + r'C:\Users\e650188\AppData\Local\graphviz238\bin'
 
 dot = gv.Digraph(node_attr={'shape': 'box'},edge_attr={'dir':'forward'},graph_attr={'rankdir':'RL'})
@@ -65,42 +65,61 @@ dot = gv.Digraph(node_attr={'shape': 'box'},edge_attr={'dir':'forward'},graph_at
 for num in range(df.shape[0]):
     dot.edge(df.iloc[num]['ORG']+'  '+str(df.iloc[num]['R_num']),df.iloc[num]['name']+'  '+str(df.iloc[num]['level']))
 
-dot.render(r'C:\Users\e650188\Desktop\automation_files\test-output\round-table_{}.gv'.format(str(datetime.datetime.now())[:-7].replace(":","-")), view=True)
+dot.render(r'C:\Users\e650188\Desktop\automation_files\test-output\{}_{}.gv'.format(mart_name,(str(datetime.datetime.now())[:-7]).replace(":","-")), view=True)
 
-################## DM element #################
+print('DM formula')################### DM formula #################
+dfcol_param=['DM_Name','Parameter','Default_Value'] # if this list changes often, will need a dict
+df_param=pd.DataFrame(columns=dfcol_param)
 
-df1=pd.DataFrame([])
+dfcol_element = ['DM_Name','Element','status','alias','key','Formula','displayName']  # if this list changes often, will need a dict
+df_element=pd.DataFrame(columns=dfcol_element)
+
+dfcol_join = ['DM_Name','alias','Source_Element','Target_Element','Main_Table(Target)']
+df_join=pd.DataFrame(columns=dfcol_join)
+
+dfcol_filter=['DM_Name','Filter'] # if this list changes often, will need a dict
+df_filter=pd.DataFrame(columns=dfcol_filter)
+
 for r,d,files in os.walk(path):
     for file in files:
         if '.xml' in file:  # only opens up xml files
             with open(os.path.join(r,file), 'r') as xml_file: # do not decode utf-8
                 etree = ET.parse(xml_file)
-            for element in ['Element']:  #  TBC 'Formula','FilterExpression','JoinElement'
-                doc_df1 = pd.DataFrame(list(iter_docs(etree.getroot(),element)))
-                if not doc_df1.empty:
-                    doc_df1['ESP_Table_Name']=file[re.search('Response_',file).span()[1]:-4]
-                    doc_df1['R_num']=int(file[:re.search('Response_',file).span()[0]-1])
-                    df1=pd.concat([df1,doc_df1],axis=0)
 
-df1=df1[['status','ESP_Table_Name','alias','name','displayName','key','dataType','length','physicalTableName','physicalColumnName','rollupFunction','sourceAlias','lookupCategory','lookupElement','R_num']]
-
-################### DM formula #################
-
-def get_value_of_node(node):
-    return node.text if node is not None else nan
-
-df2=pd.DataFrame([])
-for r,d,files in os.walk(path):
-    for file in files:
-        if '.xml' in file:  # only opens up xml files
-            with open(os.path.join(r,file), 'r') as xml_file: # do not decode utf-8
-                etree = ET.parse(xml_file)
-            dfcols = ['Element', 'Formula', 'FilterExpression', 'JoinElement']
-            df_xml=pd.DataFrame(columns=dfcols)
             
-            for node in etree.getroot():
-                Element=node.attrib.get('name')
-                Formula=node.find('Formula')
-                FilterExpression=node.find('FilterExpression')
-                JoinElement=node.find('JoinElement')
-                df_xml= df_xml.append(pd.Series([Element, get_value_of_node(Formula), get_value_of_node(FilterExpression),get_value_of_node(JoinElement)], index=dfcols),ignore_index=True)
+            for DataMart in etree.getroot():  # DataMart
+                for item in DataMart.findall('Parameters/Parameter'):
+                    df_param=df_param.append(pd.Series([DataMart.get('name'),
+                          item.get('name'),
+                          (None if item.find('DefaultValue') is None else item.find('DefaultValue').text)
+                          ],index=dfcol_param),ignore_index=True)
+
+                for item in DataMart.findall('Elements/Element'): # --element level
+                    df_element=df_element.append(pd.Series([DataMart.get('name'),
+                          item.get('name'),
+                          item.get('status'),
+                          item.get('alias'),
+                          item.get('key'),
+                          (None if item.find('Formula') is None else item.find('Formula').text),
+                          item.get('displayName')
+                          ],index=dfcol_element),ignore_index=True)
+    
+                for item in DataMart.findall('FilterCondition'):  # Filter
+                    df_filter=df_filter.append(pd.Series([DataMart.get('name'),
+                          (None if item.find('FilterExpression') is None else item.find('FilterExpression').text)
+                          ],index=dfcol_filter),ignore_index=True)
+                    
+                for item in DataMart.findall('JoinedTable'):  # Joined Table
+                    df_join=df_join.append(pd.Series([DataMart.get('name'),
+                          item.get('alias'),
+                          None if item.find('JoinElement') is None else item.find('JoinElement').get('sourceElementAlias'),
+                          None if item.find('JoinElement') is None else item.find('JoinElement').get('targetElementAlias'),
+                          None if item.find('JoinElement') is None else item.find('JoinElement').get('targetTable')
+                          ],index=dfcol_join),ignore_index=True)
+
+writer = pd.ExcelWriter(r'C:\Users\e650188\Desktop\automation_files\test-output\{}_output.xlsx'.format(mart_name))
+df_element.to_excel(writer,'Elements',index=False)
+df_join.to_excel(writer,'JoinTable',index=False)
+df_filter.to_excel(writer,'Filter',index=False)
+df_param.to_excel(writer,'Parameters',index=False)
+writer.save()
